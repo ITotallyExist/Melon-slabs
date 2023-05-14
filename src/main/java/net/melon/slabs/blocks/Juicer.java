34a2f1529ac795dbfbef8551c68fcee63fc3de3b
unknown <gregoirebrougher@gmail.com>
2023-05-13 18:22:45 -0700

    fixed juicer texture culling

--- src/main/java/net/melon/slabs/blocks/Juicer.java
+++ src/main/java/net/melon/slabs/blocks/Juicer.java
@@ -10,6 +10,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
@@ -29,10 +30,31 @@ import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
 
 public class Juicer extends BlockWithEntity  {
+
+    public static final VoxelShape SHAPE;
+    public static final VoxelShape SHAPE1;
+    public static final VoxelShape SHAPE2;
+    public static final VoxelShape SHAPE3;
+    public static final VoxelShape SHAPE4;
+    public static final VoxelShape SHAPE5;
+    public static final VoxelShape SHAPE6;
+    public static final VoxelShape SHAPE7;
+    public static final VoxelShape SHAPE8;
+    public static final VoxelShape SHAPE9;
+    public static final VoxelShape SHAPE10;
+    public static final VoxelShape SHAPE11;
+    public static final VoxelShape SHAPE12;
+    public static final VoxelShape SHAPE13;
+    public static final VoxelShape SHAPE14;
+    public static final VoxelShape SHAPE15;
+
     public static final DirectionProperty FACING;
     // private static final Text TITLE;
 
@@ -118,5 +140,32 @@ public class Juicer extends BlockWithEntity  {
         // TITLE = new TranslatableTextContent("container.crafting");
     }
 
+    public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
+    }
+  
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
+    }
+
+
+    static{
+        SHAPE1 = Block.createCuboidShape(0, 12, 0, 16, 16, 16);
+        SHAPE2 = Block.createCuboidShape(0, 1, 0, 4, 12, 4);
+        SHAPE3 = Block.createCuboidShape(12, 1, 0, 16, 12, 4);
+        SHAPE4 = Block.createCuboidShape(12, 1, 12, 16, 12, 16);
+        SHAPE5 = Block.createCuboidShape(0, 1, 12, 4, 12, 16);
+        SHAPE6 = Block.createCuboidShape(4, 1, 12, 6, 5, 16);
+        SHAPE7 = Block.createCuboidShape(10, 1, 12, 12, 5, 16);
+        SHAPE8 = Block.createCuboidShape(8.5, 1, 12, 10, 5, 13);
+        SHAPE9 = Block.createCuboidShape(6, 1, 12, 7.5, 5, 13);
+        SHAPE10 = Block.createCuboidShape(7.5, 1, 12, 8.5, 3, 13);
+        SHAPE11 = Block.createCuboidShape(7.5, 4, 12, 8.5, 5, 13);
+        SHAPE12 = Block.createCuboidShape(0, 1, 4, 4, 5, 12);
+        SHAPE13 = Block.createCuboidShape(12, 1, 4, 16, 5, 12);
+        SHAPE14 = Block.createCuboidShape(4, 1, 0, 12, 5, 4);
+        SHAPE15 = Block.createCuboidShape(0, 0, 0, 16, 1, 16);
+        SHAPE = VoxelShapes.union(SHAPE1, SHAPE2, SHAPE3, SHAPE4, SHAPE5, SHAPE6, SHAPE7, SHAPE8, SHAPE9, SHAPE10);
+    }
 
 }
