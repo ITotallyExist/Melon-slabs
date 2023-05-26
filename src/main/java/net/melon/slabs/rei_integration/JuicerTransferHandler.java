8376765122d5d7b50a23eb2c26a9a0112a8b50b2
unknown <gregoirebrougher@gmail.com>
2023-05-25 21:08:16 -0700

    changes to transferhandler to make it more robust

--- src/main/java/net/melon/slabs/rei_integration/JuicerTransferHandler.java
+++ src/main/java/net/melon/slabs/rei_integration/JuicerTransferHandler.java
@@ -3,13 +3,24 @@ package net.melon.slabs.rei_integration;
 import java.util.ArrayList;
 import java.util.List;
 
+import me.shedaniel.rei.api.client.ClientHelper;
 import me.shedaniel.rei.api.client.registry.transfer.TransferHandler;
 import me.shedaniel.rei.api.common.entry.EntryIngredient;
+import me.shedaniel.rei.api.common.transfer.info.MenuInfo;
+import me.shedaniel.rei.api.common.transfer.info.MenuInfoContext;
+import me.shedaniel.rei.api.common.transfer.info.MenuInfoRegistry;
+import me.shedaniel.rei.api.common.transfer.info.MenuTransferException;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.display.Display;
 import net.melon.slabs.packets.JuicerPacketsHandler;
 import net.melon.slabs.screens.JuicerScreen;
 import net.melon.slabs.screens.JuicerScreenHandler;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.text.Text;
 
@@ -18,6 +29,31 @@ public class JuicerTransferHandler implements TransferHandler{
     @Override
     public Result handle(Context context) {
         boolean applicable;
+
+        Display display = context.getDisplay();
+        HandledScreen<?> handledScreen = context.getContainerScreen();
+        if (handledScreen == null) {
+            return Result.createNotApplicable();
+        }
+        ScreenHandler menu = context.getMenu();
+        MenuInfoContext<ScreenHandler, PlayerEntity, Display> menuInfoContext = ofContext(menu, display);
+        MenuInfo<ScreenHandler, Display> menuInfo = MenuInfoRegistry.getInstance().getClient(display, menuInfoContext, menu);
+        if (menuInfo == null) {
+            return Result.createNotApplicable();
+        }
+        try {
+            menuInfo.validate(menuInfoContext);
+        } catch (MenuTransferException e) {
+            if (e.isApplicable()) {
+                return Result.createFailed(e.getError());
+            } else {
+                return Result.createNotApplicable();
+            }
+        }
+
+        if (!ClientHelper.getInstance().canUseMovePackets()) {
+            return Result.createFailed(Text.translatable("error.rei.not.on.server"));
+        }
 
         applicable =  context.getContainerScreen() instanceof JuicerScreen;
 
@@ -111,6 +147,30 @@ public class JuicerTransferHandler implements TransferHandler{
         
 
         return (new ResultImpl (applicable));
+    }
+
+    private static MenuInfoContext<ScreenHandler, PlayerEntity, Display> ofContext(ScreenHandler menu, Display display) {
+        return new MenuInfoContext<ScreenHandler, PlayerEntity, Display>() {
+            @Override
+            public ScreenHandler getMenu() {
+                return menu;
+            }
+            
+            @Override
+            public PlayerEntity getPlayerEntity() {
+                return MinecraftClient.getInstance().player;
+            }
+            
+            @Override
+            public CategoryIdentifier<Display> getCategoryIdentifier() {
+                return (CategoryIdentifier<Display>) display.getCategoryIdentifier();
+            }
+            
+            @Override
+            public Display getDisplay() {
+                return display;
+            }
+        };
     }
     
 }
