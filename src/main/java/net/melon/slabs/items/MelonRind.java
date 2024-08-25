07b6dc330712666d0aaf4f76ed75c75c5f603280
Greg <gregoirebrougher@gmail.com>
2024-08-24 18:05:58 -0700

    melon rind can now be applied to head via dispenser

--- src/main/java/net/melon/slabs/items/MelonRind.java
+++ src/main/java/net/melon/slabs/items/MelonRind.java
@@ -2,15 +2,26 @@ package net.melon.slabs.items;
 
 import net.melon.slabs.blocks.MelonSlabsBlocks;
 import net.melon.slabs.text.MelonSlabsText;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Equipment;
 import net.minecraft.item.ItemStack;
+import net.minecraft.predicate.entity.EntityPredicates;
 import net.minecraft.world.World;
+import net.minecraft.block.DispenserBlock;
+import net.minecraft.block.dispenser.DispenserBehavior;
+import net.minecraft.block.dispenser.ItemDispenserBehavior;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.text.TextColor;
+import net.minecraft.util.math.BlockPointer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 
 import org.jetbrains.annotations.Nullable;
 
@@ -19,9 +30,16 @@ import java.util.List;
 //this class exists to handle the intricacies of wearing a melon rind as a hard hat
 
 public class MelonRind extends BlockItem implements Equipment{
+    public static final DispenserBehavior DISPENSER_BEHAVIOR = new ItemDispenserBehavior(){
+        @Override
+        protected ItemStack dispenseSilently(BlockPointer pointer, ItemStack stack) {
+            return ArmorItem.dispenseArmor(pointer, stack) ? stack : super.dispenseSilently(pointer, stack);
+        }
+    };
 
     public MelonRind(Settings settings) {
         super(MelonSlabsBlocks.MELON_RIND, settings);
+        DispenserBlock.registerBehavior(this, DISPENSER_BEHAVIOR);
     }
 
     public EquipmentSlot getSlotType() {
@@ -33,5 +51,22 @@ public class MelonRind extends BlockItem implements Equipment{
         tooltip.add(Text.translatable("melonslabs.melon_rind_tooltip_1").setStyle(MelonSlabsText.ANNOTATION_STYLE));
         tooltip.add(Text.translatable("melonslabs.melon_rind_tooltip_2").setStyle(MelonSlabsText.ANNOTATION_STYLE));
         super.appendTooltip(stack, world, tooltip, context);
+    }
+
+    public static boolean dispenseArmor(BlockPointer pointer, ItemStack armor) {
+        BlockPos blockPos = pointer.pos().offset(pointer.state().get(DispenserBlock.FACING));
+        List<LivingEntity> list = pointer.world().getEntitiesByClass(LivingEntity.class, new Box(blockPos), EntityPredicates.EXCEPT_SPECTATOR.and(new EntityPredicates.Equipable(armor)));
+        if (list.isEmpty()) {
+            return false;
+        }
+        LivingEntity livingEntity = (LivingEntity)list.get(0);
+        EquipmentSlot equipmentSlot = MobEntity.getPreferredEquipmentSlot(armor);
+        ItemStack itemStack = armor.split(1);
+        livingEntity.equipStack(equipmentSlot, itemStack);
+        if (livingEntity instanceof MobEntity) {
+            ((MobEntity)livingEntity).setEquipmentDropChance(equipmentSlot, 2.0f);
+            ((MobEntity)livingEntity).setPersistent();
+        }
+        return true;
     }
 }
