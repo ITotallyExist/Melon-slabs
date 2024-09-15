2c0fd0f0cffa5fd6f89d2c14ceb77dcae90627ae
Greg <gregoirebrougher@gmail.com>
2024-09-15 14:47:26 -0700

    juicer recipe serializer updated to 1.20.5

--- src/main/java/net/melon/slabs/screens/JuicerRecipeSerializer.java
+++ src/main/java/net/melon/slabs/screens/JuicerRecipeSerializer.java
@@ -13,17 +13,21 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
+import io.netty.buffer.ByteBuf;
 import net.melon.slabs.screens.JuicerRecipe.JuicerRecipeJsonFormat;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.RecipeSerializer;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.dynamic.Codecs;
+import net.minecraft.util.math.BlockPos;
 
 import java.util.Optional;
 
@@ -44,39 +48,50 @@ public class JuicerRecipeSerializer implements RecipeSerializer<JuicerRecipe>{
     private static final MapCodec<Ingredient> CODEC_IN_BOTTLE = Ingredient.ALLOW_EMPTY_CODEC.fieldOf("bottleInput");
     private static final MapCodec<ItemStack> CODEC_OUT = ItemStack.CODEC.fieldOf("output");
 
-    public static final Codec<JuicerRecipe> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+    public static final MapCodec<JuicerRecipe> CODEC = RecordCodecBuilder.create(instance -> instance.group(
         CODEC_OUT.forGetter(JuicerRecipe::getOutput),
         CODEC_IN_A.forGetter(JuicerRecipe::getInputA),
         CODEC_IN_B.forGetter(JuicerRecipe::getInputB),
         CODEC_IN_C.forGetter(JuicerRecipe::getInputC),
         CODEC_IN_BOTTLE.forGetter(JuicerRecipe::getBottleInput)
-    ).apply(instance, JuicerRecipe::new));
-    
-    @Override
-    // Turns Recipe into PacketByteBuf
-    public void write(PacketByteBuf packetData, JuicerRecipe recipe) {
-        recipe.getInputA().write(packetData);
-        recipe.getInputB().write(packetData);
-        recipe.getInputC().write(packetData);
-        recipe.getBottleInput().write(packetData);
-        packetData.writeItemStack(recipe.getOutput());
-    }
+    ).apply(instance, JuicerRecipe::new)).fieldOf("melonslabs:juicer_recipe");
+
+
+    public static final PacketCodec<RegistryByteBuf, JuicerRecipe> PACKET_CODEC = new PacketCodec<RegistryByteBuf, JuicerRecipe>() {
+		public JuicerRecipe decode(RegistryByteBuf byteBuf) {
+			Ingredient inputA = Ingredient.PACKET_CODEC.decode(byteBuf);
+            Ingredient inputB = Ingredient.PACKET_CODEC.decode(byteBuf);
+            Ingredient inputC = Ingredient.PACKET_CODEC.decode(byteBuf);
+            Ingredient bottleInput = Ingredient.PACKET_CODEC.decode(byteBuf);
+            ItemStack output = ItemStack.PACKET_CODEC.decode(byteBuf);
+            return new JuicerRecipe(output, inputA, inputB, inputC, bottleInput);
+		}
+
+		public void encode(RegistryByteBuf byteBuf, JuicerRecipe recipe) {
+            Ingredient.PACKET_CODEC.encode(byteBuf, recipe.getInputA());
+            Ingredient.PACKET_CODEC.encode(byteBuf, recipe.getInputB());
+            Ingredient.PACKET_CODEC.encode(byteBuf, recipe.getInputC());
+            Ingredient.PACKET_CODEC.encode(byteBuf, recipe.getBottleInput());
+            ItemStack.PACKET_CODEC.encode(byteBuf, recipe.getOutput());
+		}
+	};
 
 
     @Override
     
-    public Codec<JuicerRecipe> codec() {
+    public MapCodec<JuicerRecipe> codec() {
         return CODEC;
     }
 
+
     @Override
-    // Turns PacketByteBuf into Recipe
-    public JuicerRecipe read(PacketByteBuf packetData) {
-        Ingredient inputA = Ingredient.fromPacket(packetData);
-        Ingredient inputB = Ingredient.fromPacket(packetData);
-        Ingredient inputC = Ingredient.fromPacket(packetData);
-        Ingredient bottleInput = Ingredient.fromPacket(packetData);
-        ItemStack output = packetData.readItemStack();
-        return new JuicerRecipe(output, inputA, inputB, inputC, bottleInput);
+    //create a codec that turns a registrybytebuf into a juicer recipe and vice versa
+    public PacketCodec<RegistryByteBuf, JuicerRecipe> packetCodec() {
+
+        
+        // TODO Auto-generated method stub
+
+
+        throw new UnsupportedOperationException("Unimplemented method 'packetCodec'");
     }
 }
